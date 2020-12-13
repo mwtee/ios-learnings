35020f97c0ecbe52a9bc4a3f040b245c61af4194
Ming Tee <ming.tee@flybuys.com.au>
2020-12-13 15:39:33 +1100

    Introduce MapView templated app

---
+++ MapView/MapView/MapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewApp.swift
+//  MapView
+//
+//  Created by Ming Tee on 13/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
