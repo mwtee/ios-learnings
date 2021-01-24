bed9c225d85d8cad276cac6b2b20f6ff8fef3eeb
Ming Tee <ming.tee@flybuys.com.au>
2021-01-24 22:20:51 +1100

    Implement basic menu details screen with an image , label, header and description

--- iDine/iDine/ItemRow.swift
+++ iDine/iDine/ItemRow.swift
@@ -14,26 +14,28 @@ struct ItemRow: View {
     let item: MenuItem
 
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
-
-            VStack(alignment: .leading) {
-                Text(item.name)
-                Text("$\(item.price)")
-            }
-
-            Spacer()
-
-            ForEach(item.restrictions, id: \.self) { restriction in
-                Text(restriction)
-                    .font(.caption)
-                    .fontWeight(.black)
-                    .padding(5)
-                    .background(Self.colors[restriction, default: .black])
+        NavigationLink(destination: ItemDetails(item: item)) {
+            HStack {
+                Image(item.thumbnailImage)
                     .clipShape(Circle())
-                    .foregroundColor(.white)
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                    Text("$\(item.price)")
+                }
+
+                Spacer()
+
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
             }
         }
     }
