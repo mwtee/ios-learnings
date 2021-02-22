038c17672fd5dbe171cbe7bd174ab78532a1f8d6
Ming Wei Tee <mingwei.tee@gmail.com>
2021-02-22 18:01:30 +1100

    Introduce CategoryHome view

---
+++ MapView/MapView/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  MapView
+//
+//  Created by Ming Wei Tee on 2/22/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+      NavigationView {
+        Text("Hello World")
+          .navigationTitle("Featured")
+      }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+      CategoryHome()
+    }
+}
