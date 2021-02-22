18a8bb5b2aab351701f115a3dba07fe8eca151ef
Ming Wei Tee <mingwei.tee@gmail.com>
2021-02-22 18:40:24 +1100

    Introduce templated category row

---
+++ ComplexUI/Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Ming Wei Tee on 2/22/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
