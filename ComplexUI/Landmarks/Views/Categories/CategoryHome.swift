f4d8a4176d71aef36d6e7a0035656c6fe3482fe2
Ming Wei Tee <mingwei.tee@gmail.com>
2021-02-22 18:17:04 +1100

    Introduce category home

---
+++ ComplexUI/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Ming Wei Tee on 2/22/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
