6e65619d6a505920d9179fae5be45d517ede6d41
Ming Wei Tee <mingwei.tee@gmail.com>
2021-02-24 22:27:57 +1100

    Introduce edit button for profile view

--- WorkingWithUiComponents/Landmarks/Views/ProfileHost.swift
+++ WorkingWithUiComponents/Landmarks/Views/ProfileHost.swift
@@ -1,19 +1,22 @@
-//
-//  ProfileHost.swift
-//  Landmarks
-//
-//  Created by Ming Wei Tee on 2/24/21.
-//  Copyright © 2021 Apple. All rights reserved.
-//
-
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                Spacer()
+                EditButton()
+            }
+
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
         }
         .padding()
     }
@@ -22,5 +25,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
